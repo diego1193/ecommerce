8d04af5f31debb27515f57e4ae741666f48ae56b
diego1193 <diegocabrera1193@gmail.com>
2024-05-07 01:42:43 -0500

    new

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ic-vl^w$r(+2u5z!v!=$x=)027+7sw3c8#ydb2#j$^lgejd0sg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
